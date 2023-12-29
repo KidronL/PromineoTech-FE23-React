4bf86529a2c9c9f9e9fc68adb0aaf806fd92df37
Kidron Lightfoot <38261315+KidronL@users.noreply.github.com>
2023-12-28 18:55:44 -0800

    commit to add components and comments

---
+++ week13/src/components/login.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+export default class Login extends React.Component {
+    //creating the function for the login. Added buttons for a password reset and to create an account.
+    render() {
+        return (
+            //wrapping the form in a div to be styled
+        <div id="formDiv" className="container-sm border rounded bg-secondary p-4 position-absolute top-50 start-50 translate-middle align-items-center">
+            <h2 className="pb-4">Login</h2>
+            <form id="loginForm align-items-center">
+                <div className="mb-3">
+                    <label htmlFor="emlInpt" className="form-label">Email Address</label>
+                    <input type="email" className="form-control" id='emlInpt'></input>
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="pwInpt" className="form-label">Password</label>
+                    <input type="password" className="form-control mb-4" id='pwInpt'></input>
+                </div>
+                <button type="submit" className="btn btn-primary mb-3">Log In</button>
+            </form>
+            <button className="btn btn-warning mb-3">Forgot Password?</button><br />
+            <button className="btn btn-success-emphasis">Create Account</button>
+        </div>
+        )
+    }
+}
--- week13/src/components/navbar.js
+++ week13/src/components/navbar.js
@@ -1,8 +1,22 @@
 import React from 'react'; 
 
 export default class Navbar extends React.Component {
+    //Creating the funtion for the navbar with an option for help. not much on the navbar as in this instance the user has not logged in yet.
     render () {
-        return React.createElement('nav')
+        return (
+            <nav id="navBar" className="navbar navbar-expand-lg bg-dark">
+                <div className="container-fluid">
+                    <a className="navbar-brand fs-2 text-white" href="#">KL</a>
+                        <div className="navbar nav">
+                            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                                <li className="nav-item">
+                                    <a className="nav-link text-white" href='#'>Help</a>
+                                </li>
+                            </ul>     
+                        </div>
+                </div>
+            </nav>
+        )
     }
 }
 
